3dd97a7eb6107e71de0aa11cc3f57eaedbb4a8fa
Mikey Holland <hello@mikeyholland.com>
2016-09-27 17:14:00 +0100

    homepage styled, europa typekit font added

--- assets/js/components/CreateFeed.jsx
+++ assets/js/components/CreateFeed.jsx
@@ -3,15 +3,26 @@ var React = require('react');
 var CreateFeed = React.createClass({
     render: function() {
         return (
-            <div>
-                <h1>Make timecoded notes on any video</h1>
-                <form className="loginForm" onSubmit={this.props.submitHandler}>
-                    <input type="text" placeholder="Enter your YouTube or Vimeo URL" value={this.props.value} onChange={this.props.changeHandler} />
-                    <input type="submit" value="Create Feed"/>
+            <main className="hero">
+                <h1>Make timecoded notes on <span className="nowrap">any video</span>.</h1>
+                <p>Plus it&apos;s free and unlimited. No account required.</p>
+                <form className="createFeed "onSubmit={this.props.submitHandler}>
+                    <div className="o-layout o-layout--flush">
+                        <div className="o-layout__item u-4/5@tablet">
+                            <input type="url" placeholder="Paste a YouTube or Vimeo URL" value={this.props.value} onChange={this.props.changeHandler} />
+                        </div>
+                        <div className="o-layout__item u-1/5@tablet">
+                            <input type="submit" value="Create" className="c-btn c-btn--primary"/>
+                        </div>
+                    </div>
                 </form>
+                <div className="o-box">
+                    <a href="#" className="c-btn c-btn--ghost u-margin-bottom">View Demo</a>
+                    <a href="#" className="c-btn u-margin-bottom">or Learn More</a>
+                </div>
                 <div>{this.props.details}</div>
                 <label>{this.props.error}</label>
-            </div>
+            </main>
         );
     }
 });
