804ba56d50a6151dbcd46696ab23f75d5b434ba2
Mikey Holland <hello@mikeyholland.com>
2016-10-11 13:26:11 +0100

    moved content markup from createFeed to home, deleted old create feed

--- assets/js/components/CreateFeed.jsx
+++ assets/js/components/CreateFeed.jsx
@@ -3,26 +3,16 @@ var React = require('react');
 var CreateFeed = React.createClass({
     render: function() {
         return (
-            <main className="hero">
-                <h1>Make timecoded notes on <span className="nowrap">any video</span>.</h1>
-                <p>Plus it&apos;s free and unlimited. No account required.</p>
-                <form className="form--single" onSubmit={this.props.submitHandler}>
-                    <div className="o-layout o-layout--flush">
-                        <div className="o-layout__item u-4/5@tablet">
-                            <input type="url" className="form--single__input" placeholder="Paste a YouTube or Vimeo URL" value={this.props.value} onChange={this.props.changeHandler} />
-                        </div>
-                        <div className="o-layout__item u-1/5@tablet">
-                            <input type="submit" value="Create" className="form--single__submit o-btn o-btn--primary"/>
-                        </div>
+            <form className="form--single" onSubmit={this.props.submitHandler}>
+                <div className="o-layout o-layout--flush">
+                    <div className="o-layout__item u-4/5@tablet">
+                        <input type="url" className="form--single__input" placeholder="Paste a YouTube or Vimeo URL" value={this.props.value} onChange={this.props.changeHandler} />
                     </div>
-                </form>
-                <div className="o-box">
-                    <a href="#" className="o-btn o-btn--ghost u-margin-bottom">View Demo</a>
-                    <a href="#" className="o-btn u-margin-bottom">or Learn More</a>
+                    <div className="o-layout__item u-1/5@tablet">
+                        <input type="submit" value="Create" className="form--single__submit o-btn o-btn--primary"/>
+                    </div>
                 </div>
-                <div>{this.props.details}</div>
-                <label>{this.props.error}</label>
-            </main>
+            </form>
         );
     }
 });
--- assets/js/views/Home.jsx
+++ assets/js/views/Home.jsx
@@ -9,7 +9,15 @@ var Home = React.createClass({
         return ( 
             <div className="homePage">
                 <HeaderContainer isHomepage={true} />
-                <CreateFeedContainer />
+                <main className="hero">
+                    <h1>Make timecoded notes on <span className="nowrap">any video</span>.</h1>
+                    <p>Plus it&apos;s free and unlimited. No account required.</p>
+                    <CreateFeedContainer />
+                    <div className="o-box">
+                        <a href="#" className="o-btn o-btn--ghost u-margin-bottom">View Demo</a>
+                        <a href="#" className="o-btn u-margin-bottom">or Learn More</a>
+                    </div>
+                </main>
             </div>
         );
     }
