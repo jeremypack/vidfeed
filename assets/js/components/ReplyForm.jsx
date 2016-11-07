9004c84fb993174f67d563fd92222ae742930b89
Mikey Holland <hello@mikeyholland.com>
2016-11-07 20:35:03 +0000

    reply form auto focuses on mount

--- assets/js/components/ReplyForm.jsx
+++ assets/js/components/ReplyForm.jsx
@@ -2,6 +2,11 @@ var React = require('react');
 var classNames = require('classnames');
 
 var ReplyForm = React.createClass({
+    
+    componentDidMount: function() {
+        this.refs.replyInput.focus();
+    },
+
     render: function() {
         
         var submitClasses = classNames({
@@ -13,7 +18,7 @@ var ReplyForm = React.createClass({
             <div className="u-margin-top-small">
                 <form className="form--border replyForm" onSubmit={this.props.handleReplySubmit}>
                     <div className="input-with-button">
-                        <input type="text" placeholder="Write a reply" value={this.props.comment} onChange={this.props.handleReplyChange} className="input--border" />
+                        <input type="text" ref="replyInput" placeholder="Write a reply" value={this.props.comment} onChange={this.props.handleReplyChange} className="input--border" />
                         <input type="submit" className={submitClasses} value="Reply" />
                     </div>
                 </form>
