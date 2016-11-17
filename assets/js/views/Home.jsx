320688446a1baac71d8b985475b9b80893321afb
Mikey Holland <hello@mikeyholland.com>
2016-11-17 17:51:08 +0000

    footer added

--- assets/js/views/Home.jsx
+++ assets/js/views/Home.jsx
@@ -8,8 +8,6 @@ var Home = React.createClass({
     
     getInitialState: function() {
         return {
-            heroWidth:undefined,
-            heroHeight:undefined,
             playerHeight:undefined
         };
     },
@@ -24,22 +22,12 @@ var Home = React.createClass({
     },
 
     _resizeContent:function() {
-        
-        console.log(this.refs.playerHolder.clientHeight,'player');
         this.setState({
-            heroWidth:window.innerWidth,
-            heroHeight:window.innerHeight,
-            playerHeight:this.refs.playerHolder.clientHeight
-            
+            playerHeight:this.refs.playerHolder.clientHeight 
         });
     },
 
     render: function() {
-        
-        var heroStyle = {
-            width:this.state.heroWidth,
-            // height:this.state.heroHeight
-        };
 
         var vimeoUrl = "https://vimeo.com/191680754";
         var vimeoConfig = {
@@ -56,7 +44,7 @@ var Home = React.createClass({
 
         return ( 
             <div className="homePage">
-                <main className="home__hero" style={heroStyle}>
+                <main className="home__hero">
                     <HeaderContainer isHomepage={true} />
                     <h1>Simple Video <span className="nowrap">Collaboration</span>.</h1>
                     <p>Make and receive timecoded notes on any Youtube or Vimeo video, for free.</p>
@@ -81,26 +69,26 @@ var Home = React.createClass({
                             </div>
                         </div>
                     </div>
-                    <div className="home__features u-padding-top-large u-padding-bottom-large">
+                    <div className="features u-padding-top-large u-padding-bottom-large">
                         <div className="o-wrapper">
                             <div className="o-layout">
                                 <div className="o-layout__item u-1/3@tablet">
-                                    <div className="u-padding-left u-padding-right">
-                                        <div className="home__features__icon"><i className="icon icon--message"></i></div>
+                                    <div className="u-margin-bottom u-padding-left u-padding-right">
+                                        <div className="feature__icon"><i className="icon icon--message"></i></div>
                                         <h3>Share</h3>
                                         <p>Share your feed with clients collaborators and friends. They can review instantly, no account required.</p>
                                     </div>
                                 </div>
                                 <div className="o-layout__item u-1/3@tablet">
-                                    <div className="u-padding-left u-padding-right">
-                                        <div className="home__features__icon"><i className="icon icon--bubble"></i></div>
+                                    <div className="u-margin-bottom u-padding-left u-padding-right">
+                                        <div className="feature__icon"><i className="icon icon--bubble"></i></div>
                                         <h3>Comment</h3>
                                         <p>Every comment is timecoded to a precise part in the video making all feedback clear and easy to understand.</p>
                                     </div>
                                 </div>
                                 <div className="o-layout__item u-1/3@tablet">
-                                    <div className="u-padding-left u-padding-right">
-                                        <div className="home__features__icon"><i className="icon icon--user"></i></div>
+                                    <div className="u-margin-bottom u-padding-left u-padding-right">
+                                        <div className="feature__icon"><i className="icon icon--user"></i></div>
                                         <h3>Collaborate</h3>
                                         <p>Review beautifully formatted timecoded comments. Collaborate in real time or start making edits straight away.</p>
                                     </div>
@@ -119,7 +107,7 @@ var Home = React.createClass({
                                     <h4 className="testimonial__name">Riyad Barmania</h4>
                                     <h5 className="testimonial__title">Content Director at ChannelFlip</h5>
                                     <blockquote className="testimonial__quote">
-                                        <p>Vidfeed is a great tool for collaboration and saves us lots of time and energy in a very simple way. We often work with multiple stakeholders including the client, the agency, legal, and the list goes on… It used to be very complicated but Vidfeed has really simplified our process.</p>
+                                        <p>Vidfeed is a great tool for collaboration, saving us lots of time and energy in a very simple way. We often work with multiple stakeholders including the client, the agency, legal, and the list goes on… It used to be very complicated but Vidfeed has really simplified our process.</p>
                                     </blockquote>
                                 </div>
                             </div>
@@ -129,14 +117,15 @@ var Home = React.createClass({
                                     <h4 className="testimonial__name">Jamie Lennox</h4>
                                     <h5 className="testimonial__title">Creative Director at Hotwolf</h5>
                                     <blockquote className="testimonial__quote">
-                                        <p>Vidfeed has revolutionised our feedback process. It&apos;s clearer, faster, and much precise than over described emails or long conference calls with multiple voices. We can spend more time being creative and less time bogged down in epic email chains.</p>
+                                        <p>Vidfeed has revolutionised our feedback process. It&apos;s clearer, faster, and much more precise than over described emails or long conference calls with multiple voices. We can spend more time being creative and less time bogged down in epic email chains.</p>
                                     </blockquote>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </section>
-                <footer></footer>
+                <a href="mailto:theteam@vidfeed.io" className="siteFooter"> Drop us a line at <strong>theteam@vidfeed.io</strong>
+                </a>
             </div>
         );
     }
