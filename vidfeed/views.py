416b676c2bc317b60a65db4efddddf7c2e5ecda4
Aidan Hamade <aidan.hamade@gmail.com>
2016-11-18 14:23:49 +0000

    serve robots.txt

--- vidfeed/views.py
+++ vidfeed/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.shortcuts import render
 
@@ -5,3 +7,8 @@ from django.shortcuts import render
 @ensure_csrf_cookie
 def index(request):
     return render(request, 'index.html', {})
+
+
+def robots(request):
+    template = loader.get_template('robots.txt')
+    return HttpResponse(template.render({}), content_type='text/plain; charset=utf8')
