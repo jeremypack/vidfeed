86d9df2222f99dd396489fa5bad454de5d798f95
package3 <jeremycpack@gmail.com>
2016-11-21 15:58:30 +0000

    can't remember what I did

--- vidfeed/settings/local.py
+++ vidfeed/settings/local.py
@@ -5,6 +5,6 @@ from .base import *
 DEBUG = True
 LOCAL = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 COOKIE_DOMAIN = 'localhost'
 BASE_URL = 'http://localhost:8000'
