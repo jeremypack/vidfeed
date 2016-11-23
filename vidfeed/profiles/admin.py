a33d9f2ce55d31779fa6d8726ecaf52f5cbfe799
Aidan Hamade <aidan.hamade@gmail.com>
2016-11-23 12:02:59 +0000

    add feeds created count to site user admin

--- vidfeed/profiles/admin.py
+++ vidfeed/profiles/admin.py
@@ -3,7 +3,10 @@ from models import SiteUser
 
 
 class SiteUserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'date_joined', 'last_login', 'is_superuser',)
+    list_display = ('email', 'first_name', 'last_name', 'feeds_created', 'date_joined', 'last_login', 'is_superuser',)
 
+    def feeds_created(self, inst):
+        return inst.feed_set.count()
+    feeds_created.admin_order_field = 'feeds_created'
 
 admin.site.register(SiteUser, SiteUserAdmin)
