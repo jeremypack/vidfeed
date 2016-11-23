559c35bafca66c0663534a5ca472ffea549778e9
Aidan Hamade <aidan.hamade@gmail.com>
2016-11-23 12:07:04 +0000

    add comment count to feed admin

--- vidfeed/feed/admin.py
+++ vidfeed/feed/admin.py
@@ -5,10 +5,15 @@ from models import Feed, Comment, Provider
 
 # Register your models here.
 class FeedAdmin(admin.ModelAdmin):
-    list_display = ('owner', 'full_name', 'provider', 'feed_id', 'video_id',  'created',)
+    list_display = ('owner', 'full_name', 'provider', 'feed_id', 'video_id', 'comment_count', 'created',)
 
     def full_name(self, inst):
         return inst.get_full_name()
+
+    def comment_count(self, inst):
+        return inst.comment_set.count()
+    comment_count.admin_order_field = 'comment_count'
+
 
 admin.site.register(Feed, FeedAdmin)
 admin.site.register(Comment)
