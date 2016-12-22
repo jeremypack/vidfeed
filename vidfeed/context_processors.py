4dde35e28c5243cedaeb87d1525c28026d0c5722
Aidan Hamade <aidan.hamade@gmail.com>
2016-12-22 15:01:36 +0000

    add user id to window.vidfeed.user

--- vidfeed/context_processors.py
+++ vidfeed/context_processors.py
@@ -1,7 +1,20 @@
 from django.conf import settings
+from vidfeed.profiles.models import SiteUser
+
 
 def vidfeed_user(request):
+    vidfeeduser = request.COOKIES.get('vidfeeduser')
+    vidfeeduser_id = None
+    if vidfeeduser:
+        try:
+            u = SiteUser.objects.get(email=vidfeeduser)
+            vidfeeduser_id = u.id
+        except SiteUser.DoesNotExist:
+            pass
+
+
     return {
-        'vidfeeduser': request.COOKIES.get('vidfeeduser'), 
+        'vidfeeduser': request.COOKIES.get('vidfeeduser'),
+        'vidfeeduser_id': vidfeeduser_id,
         'localhost': settings.LOCAL,
     }
