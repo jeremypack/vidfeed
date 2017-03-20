338a09e9877e2b97d1ede2aa525a3868a29baece
Mikey Holland <hello@mikeyholland.com>
2017-03-20 19:29:50 +0000

    removed nav from homepage, centered logo

--- assets/js/containers/HeaderContainer.jsx
+++ assets/js/containers/HeaderContainer.jsx
@@ -47,14 +47,13 @@ const HeaderContainer = React.createClass({
 
         if (this.props.isHomepage) {
             return (
-                <header className='header header--home u-clearfix' onMouseEnter={this._mouseIn} onMouseLeave={this._mouseOut}>
+                <header className='header header--home u-clearfix text--center' onMouseEnter={this._mouseIn} onMouseLeave={this._mouseOut}>
                     <div className="o-wrapper">
                         <div className="logo">
                             <Link to={logoLink} className="logo__link">
                                 <img src={window.vidfeed.images_dir + '/logo-white.svg'} alt="Vidfeed" />
                             </Link>
                         </div>
-                        <NavigationContainer mouseOut={this.state.mouseOut} isHomepage={this.props.isHomepage} />
                     </div>
                 </header>
             )
